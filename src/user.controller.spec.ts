5621d07ae327173bf3d83b6ac0b5bde9fc2d7172
Владислав Шандыбин <vladislavsandybin@MacBook-Air.local>
2025-06-17 06:28:30 +0300

    feat(user): update functions

---
+++ src/user.controller.spec.ts
@@ -0,0 +1,19 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { UserController } from './user.controller';
+import { UserModule } from './user.module';
+
+describe('AuthController', () => {
+  let controller: UserController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [UserModule],
+    }).compile();
+
+    controller = module.get<UserController>(UserController);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+});
